f090236d4955afd8402b697cd889f6dfff29e25d
Akhil Siby <akhilsiby2000@gmail.com>
2021-05-27 21:23:13 +0530

    Script updated for legal section

--- script.js
+++ script.js
@@ -120,14 +120,14 @@ $(window).scroll(function () {
 $(document).ready(function () {
   // Add down arrow icon for collapse element which is open by default
   $(".collapse.show").each(function () {
-    $(this).prev("legal-card-head").find(".fa").addClass("fa-angle-up").removeClass("fa-angle-down");
+    $(this).prev(".legal-card-head").find(".fa").addClass("fa-angle-up").removeClass("fa-angle-down");
   });
 
   // Toggle right and down arrow icon on show hide of collapse element
   $(".collapse").on('show.bs.collapse', function () {
-    $(this).prev("legal-card-head").find(".fa").removeClass("fa-angle-down").addClass("fa-angle-up");
+    $(this).prev(".legal-card-head").find(".fa").removeClass("fa-angle-down").addClass("fa-angle-up");
   }).on('hide.bs.collapse', function () {
-    $(this).prev("legal-card-head").find(".fa").removeClass("fa-angle-up").addClass("fa-angle-down");
+    $(this).prev(".legal-card-head").find(".fa").removeClass("fa-angle-up").addClass("fa-angle-down");
   });
 });
 // Legal Notice End
