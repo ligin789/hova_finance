6b8b63977687f2e9b02b18b1d9b91e6ced96d813
Akhil Siby <akhilsiby2000@gmail.com>
2021-05-27 09:56:01 +0530

    Added Navbar active change on scroll

--- script.js
+++ script.js
@@ -98,40 +98,20 @@ function stickfunction() {
 }
 
 
-//change nav according to scroll
-// Cache selectors
-var lastId,
- topMenu = $("#navbarText"),
- topMenuHeight = topMenu.outerHeight()+1,
- // All list items
- menuItems = topMenu.find("a"),
- // Anchors corresponding to menu items
- scrollItems = menuItems.map(function(){
-   var item = $($(this).attr("href"));
-    if (item.length) { return item; }
- });
+/* Code for changing active 
+            link on Scrolling */
+$(window).scroll(function () {
+  var distance = $(window).scrollTop();
+  $('.page-section-for-scroll').each(function (i) {
 
+    if ($(this).position().top
+      <= distance + 250) {
 
+      $('.navbar-nav a.active')
+        .removeClass('active');
 
-// Bind to scroll
-$(window).scroll(function(){
-   // Get container scroll position
-   var fromTop = $(this).scrollTop()+topMenuHeight;
-   
-   // Get id of current scroll item
-   var cur = scrollItems.map(function(){
-     if ($(this).offset().top < fromTop)
-       return this;
-   });
-   // Get the id of the current element
-   cur = cur[cur.length-1];
-   var id = cur && cur.length ? cur[0].id : "";
-   
-   if (lastId !== id) {
-       lastId = id;
-       // Set/remove active class
-       menuItems
-         .parent().removeClass("active")
-         .end().filter("[href=#"+id+"]").parent().addClass("active");
-   }                   
-});
+      $('.navbar-nav a').eq(i)
+        .addClass('active');
+    }
+  });
+}).scroll();
